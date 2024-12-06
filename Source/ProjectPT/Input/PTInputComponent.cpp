332a3c83fb4227d62b7ea79c1c6663cdc35303c0
osy9611 <osy9611@gmail.com>
2024-12-06 10:15:48 +0900

    파일 정리

--- Source/ProjectPT/Input/PTInputComponent.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "PTInputComponent.h"
-
-UPTInputComponent::UPTInputComponent(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
-{
-}
